48774978e726c4e804772642c0ff805edc1549d8
Karl Whitford <karl@blackesquirel.com>
2019-07-17 06:30:54 -0400

    First commit
    Init commit

---
+++ Assets/Scripts/OrderSpawner2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//To spawn in unity means to Instantiate() a GameObject
+//GameObject is the Base class for all entities in Unity Scenes
+//A GameObject is a container for many different components. 
+
+
+public class OrderSpawner2 : MonoBehaviour {
+
+
+    //An array og GameObject orders. Public means it can be accessed 
+    //globally
+    public GameObject[] orders;
+    //A Vector(x,y,z) is a vector in the physical sense.
+    //It describes the position of the source from where all
+    //orders are to be issued dispatch signals.
+    public Vector3 spawnValues;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    public float spawnWait;
+    public int startWait;
+    //as long as bool is not true; continue forever.
+    public bool stop;
+    //pick a random order every X amount of seconds.
+    int randOrder;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/SecurityRoom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityRoom : MonoBehaviour
+{
+    public GameObject orders;
+    public Vector3 spawnValues;
+
+    void Start()
+    {
+        SpawnWaves();    
+    }
+
+    void SpawnWaves() {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
